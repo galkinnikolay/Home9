4fd28219a60bbde89b9b5009ae0dc2bcf47e8890
Nikolai Galkin <galkin_nikolay@icloud.com>
2023-05-24 15:37:44 +0300

    Zadacha66

---
+++ Zadacha66/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 66: Задайте значения M и N. Напишите 
+// программу, которая найдёт сумму натуральных 
+// элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+System.Console.Write("Введите m: ");
+int m = int.Parse(Console.ReadLine());
+System.Console.Write("Введите n: ");
+int n = int.Parse(Console.ReadLine());
+
+int SumNumbers(int numStart, int numEnd) 
+{
+    if (numStart == numEnd)
+    {
+        return numStart;
+    }
+    else
+    {
+        return numStart + SumNumbers(numStart + 1, numEnd);
+    }
+}
+
+int sum = SumNumbers(m, n);
+Console.WriteLine($"Сумма чисел от {m} до {n} равна {sum}");
